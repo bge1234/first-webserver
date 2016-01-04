4efb4362b5f003ba362fa4807e1d2966ff9c2bc8
Ben Ebel <bge1234@gmail.com>
2016-01-04 11:48:58 -0700

    corrected index.js

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ var routes = require('./routes');
 
 var handleRequest = function (req, res) {
   if(routes[req.url] === undefined) {
-    routes[req.url](req, res);
-  } else {
     res.end("404, no such route");
+  } else {
+    routes[req.url](req, res);
   }
 };
 
